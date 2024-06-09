3f11245b3ceaf54bdc1a2f8e6a043cbc7216ed89
VivianaRuiz14 <162775042+VivianaRuiz14@users.noreply.github.com>
2024-06-09 00:56:55 -0400

    Incluision de formulario, y respuesta autoatica

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import index,about,welcome
+from main.views import index,about,welcome, contactus
 
 urlpatterns = [
     path('', index,name="index"),
     path('about/', about, name="about"),
     path('welcome/', welcome, name="welcome"),
+    path('contactus/', contactus, name="contactus"),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.postres import postres
+from main.forms import contactform
+
 
 # Create your views here.
 def index(req):
@@ -11,4 +13,19 @@ def about(req):
     return render(req, 'about.html')
 
 def welcome(req):
-    return render(req, 'welcome.html')
+    if req.method == 'GET':
+        form = contactform()
+        context = {'form': form}
+        return render(req, 'welcome.html', context)
+    else:
+        form = contactform(req.POST)
+    if form.is_valid():
+        return redirect('/contactus')
+    context = {'form': form}
+    return render(req, 'welcome.html', context)
+
+def exito(req):
+  return HttpResponse('Eeeexito!!!!')
+
+def contactus(req):
+    return render(req, 'contactus.html')
