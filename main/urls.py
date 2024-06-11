1b49168ddf5fc75e8bebb5eef46aaa1567b35d89
VivianaRuiz14 <162775042+VivianaRuiz14@users.noreply.github.com>
2024-06-10 21:21:08 -0400

    se modifico la vista

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from main.views import index,about,welcome, contactus
+from main.views import index,about,welcome, contactus, contact
 
 urlpatterns = [
     path('', index,name="index"),
     path('about/', about, name="about"),
     path('welcome/', welcome, name="welcome"),
+    path('contact/', contact, name="contact"),
     path('contactus/', contactus, name="contactus"),
     
 ]
--- main/views.py
+++ main/views.py
@@ -7,26 +7,31 @@ from main.models import contacto
 
 # Create your views here.
 def index(req):
+    # deme mostrar todos los flanes de la base de datos
     context= {'postres':postres }
     return render(req,'index.html',context)
 
 def about(req):
     return render(req, 'about.html')
 
-def welcome(req):
+def contact(req):
     if req.method == 'GET':
         form = contactform()
         context = {'form': form}
-        return render(req, 'welcome.html', context)
+        return render(req, 'contact.html', context)
     else:
         form = contactform(req.POST)
-    if form.is_valid():
-        contacto.objects.create(
-            **form.cleaned_data
-        )
-        return redirect('/contactus')
-    context = {'form': form}
-    return render(req, 'welcome.html', context)
+        if form.is_valid():
+            contacto.objects.create(
+                **form.cleaned_data
+            )
+            return redirect('/contactus')
+        context = {'form': form}
+        return render(req, 'contact.html', context)
+def welcome(req):
+    # deme mostrar sólo los flanes privados de la base de datos
+    context= {'postres':postres }
+    return render(req,'welcome.html',context)
 
 def exito(req):
   return HttpResponse('Eeeexito!!!!')
